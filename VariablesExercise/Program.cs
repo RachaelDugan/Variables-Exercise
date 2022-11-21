29e76c4ac2e60a4e5a23bf1d107dc4270ad2591e
RachaelDugan <118020235+RachaelDugan@users.noreply.github.com>
2022-11-21 10:04:17 -0700

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -5,6 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            string myStreet = "Hollyhock Lane";
+            int streetNum = 5263;
+            char myUnit = 'Z';
+            double myGPA = 2.7;
+            decimal myIQ = 85.29877m;
+            bool notMyAddress = false;
+            Console.WriteLine($"The address entered was: {myStreet} {streetNum} {myUnit}");              
+            Console.WriteLine(notMyAddress);
+            Console.WriteLine($"My GPA and IQ are: {myGPA} {myIQ}");
         }
     }
 }
